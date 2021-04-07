6bcd013369b88a6426be8a209be80c2754f8de13
Vitor Rafael Rocha Campelo <vitorafael.r.c@gmail.com>
2021-04-07 13:59:42 -0300

    📝 Form ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [date, setDate] = useState('');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>
+          Workout Log 
+        </h1>
       </header>
+      <div className="container" style={{flexDirection: 'column'}}>
+        <label htmlFor="form">Insert an item</label>
+        <form>
+          <div>
+            <input type="number" id="time" placeholder="Time spent" value={date} onChange={(e) => setDate(e.target.value)}/>
+            <select name="works" id="work">
+              <option value="run">Run</option>
+              <option value="swimming">Swimming</option>
+              <option value="mercedes">Bike</option>
+            </select>
+          </div>
+
+          <div>
+            <input type="date" className="date-input" name="date"/>
+            <button className="add" type="submit" onClick={(e) => e.preventDefault()}>Add</button>
+          </div>
+        </form>
+        <hr style={{height:4, color: 'black'}}></hr>
+      </div>
     </div>
   );
 }
